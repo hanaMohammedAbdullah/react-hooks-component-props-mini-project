c637a2b53a11fec9aab6c6867d2a995cb58a6193
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-03-21 17:04:33 +0300

    adding all the component with there prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import ArticleList from "./ArticleList";
+import About from "./About";
 
 console.log(blogData);
 
 function App() {
   return (
-    <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+    <div className='App'>
+      <Header name={blogData.name} />
+      <About
+        image={blogData.image}
+        name={blogData.name}
+        about={blogData.about}
+      />
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
